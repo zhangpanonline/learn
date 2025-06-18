9e387c70494cbfaa43f60fe89b23566d407ecc3d
zhangpan <zhangpan.online@outlook.com>
2025-06-18 20:01:43 +0800

    nextjs

---
+++ nextjs/src/app/auth/page.tsx
@@ -0,0 +1,9 @@
+export default function Auth() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold mb-4">Auth Page</h1>
+      <p className="text-gray-600">This is the auth page</p>
+      <button className="btn w-64 rounded-full">Button</button>
+    </div>
+  )
+}
