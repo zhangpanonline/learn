ad728767d6d3f06abc9d2f1bcf383b3d31e6e987
zhangpan <zhangpan.online@outlook.com>
2025-07-02 20:04:53 +0800

    solidity

---
+++ solidity/hello.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Hello {
+    // string public msg = 'Hello Web3';
+    // int public a1 = 1 * 2**255 - 1; // 可以取到的最大数，不能超过256个比特位
+    // uint public a2 = 1 * 2**256 - 1; // 可以取到的最大数，不能超过256个比特位
+
+    // bytes2 public b2 = hex"1000";
+
+    // enum Status {
+    //     Active,
+    //     Inactive
+    // }
+
+    int[] public arr1;
+    string[] public arr2;
+    bool[] public arr3;
+    address[] public arr4;
+    bytes10[] public arr5;
+    string public str = "hello";
+
+    struct Person {
+        uint8 age;
+        bool sex;
+        string name;
+    }
+
+    Person public zhangsan = Person({age: 18, sex: false, name: "zhangsan"});
+}
