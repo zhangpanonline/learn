2a255926b2e31d989e16f77e07f9ecae312ddaf2
zhangpan <zhangpan.online@outlook.com>
2025-07-17 19:34:28 +0800

    123

--- solidity/hello.sol
+++ solidity/hello.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+
+contract ExampleContract {
+    
+}
+
 contract Hello {
     // string public msg = 'Hello Web3';
     // int public a1 = 1 * 2**255 - 1; // 可以取到的最大数，不能超过256个比特位
@@ -13,18 +18,24 @@ contract Hello {
     //     Inactive
     // }
 
-    int[] public arr1;
-    string[] public arr2;
-    bool[] public arr3;
-    address[] public arr4;
-    bytes10[] public arr5;
-    string public str = "hello";
+    // int[] public arr1;
+    // string[] public arr2;
+    // bool[] public arr3;
+    // address[] public arr4;
+    // bytes10[] public arr5;
+    // string public str = "hello";
 
-    struct Person {
-        uint8 age;
-        bool sex;
-        string name;
-    }
+    // struct Person {
+    //     uint8 age;
+    //     bool sex;
+    //     string name;
+    // }
 
-    Person public zhangsan = Person({age: 18, sex: false, name: "zhangsan"});
+    // Person public zhangsan = Person({age: 18, sex: false, name: "zhangsan"});
+
+    // uint public balance;
+
+    // function getValue() public payable returns (uint) {
+    //     return balance += msg.value;
+    // }
 }
